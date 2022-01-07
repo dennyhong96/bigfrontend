067063ed42567e15646e42588e8570ce646f00d1
Denny Hong <hong961127@gmail.com>
2022-01-06 21:01:23 -0800

    Implement race

---
+++ 31.race.ts
@@ -0,0 +1,64 @@
+type Callback<T> = (error: Error | undefined, data: T | undefined) => void;
+
+type AsyncFunc<T> = (callback: Callback<T>) => void;
+
+function race<T>(funcs: AsyncFunc<T>[]) {
+  return function (finalCallback: Callback<T>) {
+    let hasWinner = false;
+    funcs.forEach((func) => {
+      func((error, data) => {
+        if (hasWinner) return;
+        hasWinner = true;
+        if (error) return finalCallback(error, undefined);
+        return finalCallback(undefined, data);
+      });
+    });
+  };
+}
+
+function racePromise<T>(funcs: AsyncFunc<T>[]) {
+  const promisify = (fn: AsyncFunc<T>): Promise<T | undefined> => {
+    return new Promise((res, rej) => {
+      fn((error, data) => {
+        if (error) return rej(error);
+        res(data);
+      });
+    });
+  };
+
+  return function (finalCallback: Callback<T>) {
+    let hasWinner = false;
+    funcs.map(promisify).forEach((promise) => {
+      promise
+        .then((data) => {
+          if (hasWinner) return;
+          finalCallback(undefined, data);
+          hasWinner = true;
+        })
+        .catch((error) => {
+          if (hasWinner) return;
+          finalCallback(error, undefined);
+          hasWinner = true;
+        });
+    });
+  };
+}
+
+// Example
+const async1 = (callback: Callback<number>) => {
+  setTimeout(() => callback(undefined, 1), 300);
+};
+
+const async2 = (callback: Callback<number>) => {
+  setTimeout(() => callback(undefined, 2), 100);
+};
+
+const async3 = (callback: Callback<number>) => {
+  setTimeout(() => callback(undefined, 3), 200);
+};
+
+const first = race([async1, async2, async3]);
+
+first((error, data) => {
+  console.log(data); // 2, since 2 is the first to be given
+});
