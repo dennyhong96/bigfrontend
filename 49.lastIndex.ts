256f29c146a5e2a092f383513393b5781aa854cb
Denny Hong <hong961127@gmail.com>
2022-01-19 16:11:36 -0800

    Add 49.firstIndex

--- 49.lastIndex.ts
+++ 49.lastIndex.ts
@@ -3,19 +3,18 @@
  * @param {number} target
  * @return {number}
  */
-function firstIndex(arr: number[], target: number): number {
+function lastIndex(arr: number[], target: number) {
   let l = 0;
   let r = arr.length - 1;
   while (l <= r) {
     const m = l + Math.floor((r - l) / 2);
     const num = arr[m];
     if (num === target) {
-      // Keep moving i pointer left to find the first element with the same value
-      let i = m - 1;
-      while (arr[i] === arr[m] && i >= 0) {
-        i--;
+      let i = m + 1;
+      while (arr[i] === num && i < arr.length) {
+        i++;
       }
-      return i + 1;
+      return i - 1;
     }
     if (num < target) {
       l = m + 1;
@@ -26,4 +25,4 @@ function firstIndex(arr: number[], target: number): number {
   return -1;
 }
 
-console.log(firstIndex([1, 1, 2, 3, 3, 3, 4, 4, 4, 5], 4)); // 4 -> 6
+export {};
