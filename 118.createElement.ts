68613b040349d1c24c2a57f1cb5fe462ab819d59
Denny Hong <hong961127@gmail.com>
2022-02-25 15:13:50 -0800

    Implement 118.createElement

---
+++ 118.createElement.ts
@@ -0,0 +1,44 @@
+type MyNode = MyElement | string;
+interface MyElement {
+  type: string;
+  props: {
+    [key: string]: string | MyNode[];
+    children: MyNode[];
+  };
+}
+
+export function createElement(
+  type: string,
+  props: Record<string, string>,
+  ...children: MyNode[]
+): MyElement {
+  return {
+    type,
+    props: {
+      ...props,
+      children,
+    },
+  };
+}
+
+export function render(myElement: MyElement): HTMLElement {
+  const element = document.createElement(myElement.type);
+
+  // handle attributes
+  let { children, ...restProps } = myElement.props;
+  for (const [name, value] of Object.entries(restProps)) {
+    const key = name === "className" ? "class" : name;
+    element.setAttribute(key, value as string);
+  }
+
+  // handle children
+  if (!Array.isArray(children)) children = [children];
+  element.append(
+    ...children.map((child) => {
+      if (typeof child === "string") return document.createTextNode(child);
+      return render(child);
+    })
+  );
+
+  return element;
+}
